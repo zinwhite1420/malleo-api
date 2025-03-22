320524f5ca6d3a56ab34a7fac0935deff7b90ddc
zinwhite <zinwhite142@gmail.com>
2025-03-22 22:01:48 +0900

    최초 프로젝트 생성

---
+++ src/test/java/com/malleo/api/MalleoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malleo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalleoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
